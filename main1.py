acc51bd630819f8bce9effb02022ac5feca701ba
antONget Anton <a.l.ponomarev@mail.ru>
2021-10-21 17:38:21 +0400

    добавлена анимация и исправлен баг

--- main1.py
+++ main1.py
@@ -1,18 +1,19 @@
+
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from plyer import filechooser
+from kivy.animation import Animation
+from PIL import Image
 import numpy as np
 from tensorflow.keras.models import load_model  # из кераса подгружаем  метод загрузки предобученной модели
-
-from PIL import Image
-from kivy.animation import Animation
+import tensorflow as tf
 
 KV = """
 ScreenManager:
     MDScreen:
         id: home
         MDLabel:
-            text: "Загрузите изображение для классификации"
+            text: "Загрузите изображение для распоснания"
             #theme_text_color: "Custom"
             #text_color: .5, .5, .45, .8
             pos_hint: {"center_x": .5, "center_y": .93}
@@ -54,15 +55,13 @@ ScreenManager:
             text: "PRIVET"
             x_hint: 2.0
             pos_hint: {"center_x": self.x_hint, "center_y": .5}
-            font_style: "H5"
+            font_style: "H3"
 
 
 """
 
 
 class ImageClassify(MDApp):
-    model = load_model('best_model+815.h5')
-
 
     def build(self):
         return Builder.load_string(KV)
@@ -70,18 +69,16 @@ class ImageClassify(MDApp):
     def file_chooser(self):
         filechooser.open_file(on_selection=self.selected)
 
-    def selected(self, selection, model=model):
+    def selected(self, selection):
         if selection:
             self.root.ids.img.source = selection[0]
             print(selection[0])
-            pred = self.recognition(selection[0], model)
-            self.root.ids.label.text = pred
-
-        return pred
+            # self.recognition(selection[0])
 
     def anime(self, label, *args):
         anim = Animation(x_hint=.9, duration=1.5)
         anim.start(label)
+        # self.root.ids.label.text =
 
     def anima1(self, card, *args):
         if self.root.ids.button.text == "Load Image":
@@ -100,6 +97,35 @@ class ImageClassify(MDApp):
         else:
             pass
 
+    # def recognition(self, image):
+    #    # Convert the model
+    #    model = tf.lite.TFLiteConverter.from_keras_model('best_model+815.h5')  # path to the SavedModel directory
+    #    # tflite_model = converter.convert()
+    # model = load_model('best_model+815.h5')
+    # наименование класса попорядку
+    #    numClass = ['DJI_Inspire_2',
+    #                'DJI_Matrice_210-RTK',
+    #                'DJI_Matrice_600_Pro',
+    #                'DJI_Mavic_Moscow',
+    #                'DJI_Mavic_Pro_Platinum',
+    #                'DJI_Phantom_4',
+    #                'DJI_Phantom_4_Pro_Plus',
+    #                'DJI_Spark',
+    #               'Moscow_Noise']
+
+    #    img_width = 227  # Ширина изображения
+    #    img_height = 227  # Высота изображения
+
+    #    img = np.array(image)  # переводим в массив
+    #    img = img / 255.0  # нормализуем
+    #    img = np.expand_dims(img, axis=0)  # добавляем дополнительную размерность, так как НС просит размер батч сайза
+    #    listProbablyImg = model.predict(img)
+
+    #   predClass = np.argmax(listProbablyImg)
+    #    percentClass = round(listProbablyImg[0, predClass] * 100, 2)
+    #    nameClass = numClass[predClass]
+    #    # выводим информацию по распознованию в лейбл
+    #    print("This image belongs to {} class with probability {} %".format(predClass, listProbablyImg))
     def recognition(self, pathImage, model):
 
         # наименование класса попорядку
@@ -115,27 +141,20 @@ class ImageClassify(MDApp):
 
         img_width = 227  # Ширина изображения
         img_height = 227  # Высота изображения
-
         image = Image.open(pathImage)
-
-        size = (img_width, img_height)
-        image = image.resize(size)
-
         img = np.array(image)  # переводим в массив
         img = img / 255.0  # нормализуем
         img = np.expand_dims(img, axis=0)  # добавляем дополнительную размерность, так как НС просит размер батч сайза
         listProbablyImg = model.predict(img)
 
-        predImg = np.argmax(listProbablyImg)
-        percentImg = round(listProbablyImg[0, predImg] * 100, 2)
-        nameClass = numClass[predImg]
+        predClass = np.argmax(listProbablyImg)
+        percentClass = round(listProbablyImg[0, predClass] * 100, 2)
+        nameClass = numClass[predClass]
         # выводим информацию по распознованию в лейбл
+        pred = ("This image belongs to {} class #{}# with probability {} %".format(predClass, numClass[predClass], listProbablyImg[predClass],))
 
-        pred = ("Это изображение относится к {} классу #{}# с вероятностью {} %".format(predImg, nameClass,
-                                                                                        percentImg))
-        print(pred)
         return str(pred)
-
+        #self.root.ids.label.text = pred
 
 if __name__ == "__main__":
     ImageClassify().run()
